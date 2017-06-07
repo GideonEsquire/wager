e82a7b061c1855bec715fe1a07ae2bde409321e5
GideonEsquire <GideonEsquire@gmail.com>
2017-06-07 10:23:42 -0500

    added steering behaviors

--- sketch.js
+++ sketch.js
@@ -1,17 +1,44 @@
+// Vast majortiy copied from Daniel Shiffman
+// http://codingtra.in
+// Steering Text Paths
+// Video: https://www.youtube.com/watch?v=4hA7G3gup-4
+
+var font;
+var vehicles = [];
+
 function preload() {
-
+  font = loadFont('AvenirNextLTPro-Demi.otf');
   data = loadJSON("db.json");
-
 }
 
 function setup() {
+  createCanvas(windowWidth, 400);
+  background(255);
 
-  background(0);
-  fill(255);
-  text(data.Housework[2], 10, 50);
+  // with the activity variable I want to grab a random activity from data
+  activity = 'Skyler'
 
+  var points = font.textToPoints(activity, 100, 200, 192, {
+    sampleFactor: 0.25
+  });
+
+  for (var i = 0; i < points.length; i++) {
+    var pt = points[i];
+    var vehicle = new Vehicle(pt.x, pt.y);
+    vehicles.push(vehicle);
+    // stroke(255);
+    // strokeWeight(8);
+    // point(pt.x, pt.y);
+  }
 }
 
 function draw() {
-
+  background(255);
+  for (var i = 0; i < vehicles.length; i++) {
+    var v = vehicles[i];
+    v.behaviors();
+    v.update();
+    v.show();
+  }
 }
+
